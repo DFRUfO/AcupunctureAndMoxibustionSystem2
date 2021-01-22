f9decf2e11d62aa3e6762d58e63893cd3d9db405
DFRUfO <49854860+DFRUfO@users.noreply.github.com>
2021-01-22 23:22:30 +0800

    分支提交

--- AcupunctureAndMoxibustionSystem-pojo/src/main/java/net/seehope/pojo/Users.java
+++ AcupunctureAndMoxibustionSystem-pojo/src/main/java/net/seehope/pojo/Users.java
@@ -28,7 +28,7 @@ public class Users implements Serializable {
      */
     public String getId() {
 
-        System.out.println("la");return id;
+        System.out.println("adhfao");return id;
     }
 
     /**
